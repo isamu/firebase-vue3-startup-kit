c757f3c15e5401a059c258279865069476f9d394
Isamu Arimoto <isamu@to-kyo.to>
2021-02-27 20:40:05 +0900

    add onAuthStateChanged

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,21 +1,43 @@
 <template>
   <div class="layout">
     {{message}}
+    <template v-if="user.user">
+      {{user.user.displayName}}
+    </template>
     <router-view/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import {db} from "../utils/firebase";
+import { defineComponent, reactive, onMounted } from 'vue';
+import {db, auth} from "../utils/firebase";
+import firebase from "firebase/app";
+
+interface User {
+  user: firebase.User | null;
+}
 
 export default defineComponent({
   name: 'Layout',
   async setup() {
+    const user = reactive<User>({user: null});
+
+    onMounted(() => {
+      auth.onAuthStateChanged(fbuser => {
+        if (fbuser) {
+          console.log(
+            "authStateChanged:",
+          );
+          user.user = fbuser;
+        }
+      });
+    });
     const messageDoc = await db.doc("/test/message").get()
     const message = messageDoc.data();
+
     return {
       message,
+      user,
     };
   },
 });
