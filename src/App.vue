331c4557369b03b1a4893f90b497007903df0b16
Yuta Okajima <yuta.ok@gmail.com>
2021-02-28 14:41:27 -0800

    Install Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -27,31 +27,39 @@ export default {
 
     return {
       user,
-      isSiginedIn
+      isSiginedIn,
     };
-  }
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2c3e50; */
+  @apply font-sans antialiased text-center text-gray-700;
 }
 
 #nav {
-  padding: 30px;
+  /* padding: 30px; */
+  @apply p-8;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  /* font-weight: bold;
+  color: #2c3e50; */
+  @apply font-bold text-gray-700;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  /* color: #42b983; */
+  @apply text-green-600;
+}
+
+a {
+  @apply cursor-pointer;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="text-4xl font-bold my-6">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -116,25 +116,31 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "HelloWorld",
   props: {
-    msg: String
-  }
+    msg: String,
+  },
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+  /* margin: 40px 0 0; */
+  @apply mt-10;
 }
+
 ul {
-  list-style-type: none;
-  padding: 0;
+  /* list-style-type: none;
+  padding: 0; */
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+  /* display: inline-block;
+  margin: 0 10px; */
+  @apply inline-block mx-3;
 }
+
 a {
-  color: #42b983;
+  /* color: #42b983; */
+  @apply text-green-600;
 }
 </style>
