fc8ba5381d781576daf0d1d7599cb022f706c706
isamu <isamu@to-kyo.to>
2022-04-18 07:45:40 +0900

    update vue cli and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,10 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
-    extend: {
-      colors: {
-        "tailwindcss-logo-mark": "#06B6D4",
-        bluegray: colors.blueGray,
-        coolgray: colors.coolGray,
-        gray: colors.gray,
-        truegray: colors.trueGray,
-        warmgray: colors.warmGray,
-        red: colors.red,
-        orange: colors.orange,
-        amber: colors.amber,
-        yellow: colors.yellow,
-        lime: colors.lime,
-        green: colors.green,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        cyan: colors.cyan,
-        lightblue: colors.lightBlue,
-        blue: colors.blue,
-        indigo: colors.indigo,
-        violet: colors.violet,
-        purple: colors.purple,
-        fuchsia: colors.fuchsia,
-        pink: colors.pink,
-        rose: colors.rose,
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
-};
+}
