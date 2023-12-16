3bb863357fd767d41f2c9ba00094f682ecf87f59
isamu <isamu@to-kyo.to>
2023-12-17 08:52:51 +0900

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/vue3-strongly-recommended',
-    'eslint:recommended',
-    'prettier',
-    '@vue/typescript/recommended'
+  extends: [
+    "plugin:vue/vue3-strongly-recommended",
+    "eslint:recommended",
+    "prettier",
+    "@vue/typescript/recommended",
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     "vue/no-unused-vars": "error",
@@ -18,8 +18,8 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "error",
     "@typescript-eslint/no-unused-vars": "error",
     "no-unreachable": "error",
-    'vue/multi-word-component-names': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "vue/multi-word-component-names": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  content: [
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
