f56a77c6a95aa20035acbb24e4f6f0f66d12a394
isamu <isamu@to-kyo.to>
2023-12-23 06:07:27 +0900

    vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    port: 8080,
+  },
+});
