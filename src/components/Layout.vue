36ac2b6974242a9b584056fcebc49d5e52acb038
ys64 <815824+ys64@users.noreply.github.com>
2024-04-25 21:51:59 -0700

    update icons (Material Icons -> Material Symbols)

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -5,7 +5,7 @@
         <div class="bg-blue-300">
           <div class="relative flex items-center">
             <div @click="toggleMenu()" class="inline-flex h-14 w-14 flex-shrink-0 cursor-pointer items-center justify-center">
-              <span class="material-icons text-warmgray-900 text-opacity-60">menu</span>
+              <span class="material-symbols-outlined text-warmgray-900 text-opacity-60">menu</span>
             </div>
             <div class="w-full items-center">Firebase Vue3 kit</div>
             <div v-show="menu" class="fixed top-0 left-0 z-30 flex h-screen w-screen">
