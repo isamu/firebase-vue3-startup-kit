52c99c38295071f5f30d0e6e8929b62e16ec9f56
isamu <isamu@to-kyo.to>
2024-06-30 08:54:09 +0900

    flag config

---
+++ eslint.config.mjs
@@ -0,0 +1,100 @@
+import eslint from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import sonarjs from "eslint-plugin-sonarjs";
+import pluginVue from 'eslint-plugin-vue';
+import vueParser from "vue-eslint-parser";
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["src/**/*.{vue,ts}"],
+  },
+  {
+    ignores: ["lib/**/*", "*.ts"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    plugins: {
+      'typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        project: './tsconfig.json',
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module',
+      },
+    },
+  },
+  sonarjs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      indent: ["error", 2],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^__",
+          varsIgnorePattern: "^__",
+        },
+      ],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+
+      "vue/no-unused-vars": "error",
+      "vue/no-reserved-component-names": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-unreachable": "error",
+      "vue/multi-word-component-names": "off",
+      "sonarjs/cognitive-complexity": "error",
+      "sonarjs/no-duplicate-string": "error",
+      "sonarjs/elseif-without-else": "error",
+      "sonarjs/max-switch-cases": "error",
+      "sonarjs/no-all-duplicated-branches": "error",
+      "sonarjs/no-collapsible-if": "error",
+      "sonarjs/no-collection-size-mischeck": "error",
+      "sonarjs/no-duplicate-string": "error",
+      "sonarjs/no-duplicated-branches": "error",
+      "sonarjs/no-element-overwrite": "error",
+      "sonarjs/no-empty-collection": "error",
+      "sonarjs/no-extra-arguments": "error",
+      "sonarjs/no-gratuitous-expressions": "error",
+      "sonarjs/no-identical-conditions": "error",
+      "sonarjs/no-identical-expressions": "error",
+      "sonarjs/no-identical-functions": "error",
+      "sonarjs/no-ignored-return": "error",
+      "sonarjs/no-inverted-boolean-check": "error",
+      "sonarjs/no-nested-switch": "error",
+      "sonarjs/no-nested-template-literals": "error",
+      "sonarjs/no-one-iteration-loop": "error",
+      "sonarjs/no-redundant-boolean": "error",
+      "sonarjs/no-redundant-jump": "error",
+      "sonarjs/no-same-line-conditional": "error",
+      "sonarjs/no-small-switch": "error",
+      "sonarjs/no-unused-collection": "error",
+      "sonarjs/no-use-of-empty-return-value": "error",
+      "sonarjs/no-useless-catch": "error",
+      "sonarjs/non-existent-operator": "error",
+      "sonarjs/prefer-immediate-return": "error",
+      "sonarjs/prefer-object-literal": "error",
+      "sonarjs/prefer-single-boolean-return": "error",
+      "sonarjs/prefer-while": "error",
+      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+
+    },
+  },
+];
