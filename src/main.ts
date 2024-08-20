a6c9d2e836d69ae05809a693ca48d687eb98d509
isamu <isamu@to-kyo.to>
2024-08-21 07:05:11 +0900

    @unhead/vue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createHead } from "@vueuse/head";
+import { createHead } from "@unhead/vue";
 import { createI18n } from "vue-i18n";
 import App from "./App.vue";
 import "./index.css";
--- src/views/About.vue
+++ src/views/About.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { useHead } from "@vueuse/head";
+import { useHead } from "@unhead/vue";
 
 export default defineComponent({
   name: "AboutPage",
